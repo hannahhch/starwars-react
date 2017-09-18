bcc2a3681acabd50ad40186890d16483498b3a10
Hannah Hudson <hannah.hch@gmail.com>
2017-09-17 20:14:20 -0400

    form works, project finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,10 @@ export default class Form extends Component{
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.state = { pilot: "" }
+    this.state = {
+      pilot: "",
+      newPilot: ""
+    }
   }
 
   handleNameChange(e){
@@ -14,8 +17,11 @@ export default class Form extends Component{
   }
 
   handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({pilot: e.target.value});
+    e.preventDefault()
+    this.setState({
+      newPilot: this.state.pilot,
+      pilot: ""
+    });
   }
 
   render(){
@@ -24,14 +30,15 @@ export default class Form extends Component{
         <div className = "form-header">
           <h3>What is your name, pilot?</h3>
         </div>
-        <form>
-          <div className="form-group" onSubmit={this.handleSubmit}>
-            <input className="form-control col-md-3" onChange={this.handleNameChange} name="name" value={this.state.pilot} type="text"/>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group" >
+            <input className="form-control col-md-3" onChange={this.handleNameChange} name="name" value={this.state.pilot} placeholder="Enter your name" type="text"/>
           </div>
           <div className="form-group pull-right">
             <input className="btn btn-primary btn-lg" type="submit" value="Submit"/>
           </div>
         </form>
+        <h4>{this.state.newPilot}</h4>
       </div>
     )
   }
